d73270196960a5ca37ad96cda09f9d4dcb09ffa1
Phalkun <lehuydiep4@gmail.com>
2025-12-08 17:01:00 +0700

    Gradle Init

---
+++ sacabam-clicker-be/src/test/java/org/sacabam/sacabamclickerbe/SacabamClickerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sacabam.sacabamclickerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacabamClickerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
